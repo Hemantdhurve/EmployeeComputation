6a498780c11118a76a4334cad6356b11fa0bd823
Hemantdhurve <hemantdhurve007@gmail.com>
2022-07-11 22:14:34 +0530

    Added EmployeePartTime file

---
+++ UC3_EmployeePartTime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Program;
+
+class EmployeePartTime
+{
+    static void Main(string[] args)
+    {
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
+        int Emp_Rate_Per_Hr = 20;
+
+        int EmpHrs = 0;
+        int EmpWage = 0;
+        Random random = new Random();
+        int empCheck = random.Next(0, 3);
+        if (empCheck == IS_FULL_TIME)
+        {
+            empCheck = 8;
+        }
+        else if (empCheck == IS_PART_TIME)
+        {
+            EmpHrs = 4;
+        }
+        else
+        {
+            EmpHrs = 0;
+        }
+        EmpWage = EmpHrs * Emp_Rate_Per_Hr;
+        Console.WriteLine("Emp Wage :" + EmpWage);
+    }
+}
