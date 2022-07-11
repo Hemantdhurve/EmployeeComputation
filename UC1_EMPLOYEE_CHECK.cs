d8ccbee16b8a88b10cd0e7f75a2a1368d8315f48
Hemantdhurve <hemantdhurve007@gmail.com>
2022-07-11 22:04:55 +0530

    Added EmployeeCheck file

---
+++ UC1_EMPLOYEE_CHECK.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Program
+{
+    class UC1_Employee_Check
+    {
+        static void main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
